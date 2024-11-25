796e77c38cbd8b64004c7cc80ee5a4f10302175d
Shivam Bansal <shivambnsal@gmail.com>
2024-11-26 01:05:11 +0530

    Add size, getHead, getTail, at, pop, contains, find methods

--- modules/linkedLists.js
+++ modules/linkedLists.js
@@ -1,6 +1,6 @@
 import { Node } from "./node.js";
 
-class LinkedList {
+export class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -30,17 +30,93 @@ class LinkedList {
     }
   }
 
-  toString() {
-    let resultString = "";
+  size() {
+    let currentNode = this.head,
+      numberOfNodes = 0;
 
+    while (currentNode !== null) {
+      numberOfNodes += 1;
+      currentNode = currentNode.nextNode;
+    }
+    return numberOfNodes;
+  }
+
+  getHead() {
+    if (this.head === null) return null;
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  at(index) {
+    if (this.head === null) return "Index out of range! The list is empty.";
+
+    let iterator = 0;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (iterator === index) {
+        return currentNode.value;
+      }
+      iterator++;
+      currentNode = currentNode.nextNode;
+    }
+    return `Index out of range! . Size of list is ${this.size()}`;
+  }
+
+  pop() {
+    if (this.head === null) return "The list is empty.";
+
+    let previousNode = this.head;
+    while (previousNode.nextNode !== this.tail)
+      previousNode = previousNode.nextNode;
+
+    previousNode.nextNode = null;
+    this.tail = previousNode;
+  }
+
+  contains(value) {
+    if (this.head === null) return false;
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value === value) return true;
+      currentNode = currentNode.nextNode;
+    }
+
+    return false;
+  }
+
+  find(value) {
+    if (this.head === null) return null;
+    let index = 0;
+
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return index;
+      }
+      index++;
+      currentNode = currentNode.nextNode;
+    }
+
+    return null;
+  }
+
+  toString() {
+    if (this.head === null) return "The list is empty.";
+
+    let resultString = "";
     let currentNode = this.head;
 
     while (currentNode !== null) {
       resultString += `( ${currentNode.value} ) --> `;
       currentNode = currentNode.nextNode;
     }
+
     resultString += "null";
-    console.log(resultString);
+    return resultString;
   }
 }
 
@@ -49,4 +125,6 @@ let ll = new LinkedList();
 ll.prepend(3);
 ll.prepend(99);
 ll.append(100);
-ll.toString();
+console.log(ll.toString());
+console.log(ll.contains(0));
+console.log(ll.find(99));
