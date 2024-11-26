9e92db1a474b4add9fdaad1c61a5f7624b83b0d4
Shivam Bansal <shivambnsal@gmail.com>
2024-11-27 01:27:19 +0530

    Create test cases

---
+++ index.js
@@ -0,0 +1,26 @@
+import { LinkedList } from "./modules/linkedLists.js";
+
+// Tests
+const linkedList = new LinkedList();
+
+linkedList.prepend("test1");
+linkedList.append("test2");
+linkedList.append("test3");
+console.log(linkedList.toString()); // (test1) -> (test2) -> (test3) -> null
+console.log(linkedList.size()); // 3
+console.log(linkedList.getHead()); // return head Node
+console.log(linkedList.getTail()); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(2)); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(4)); // There is no item for this index
+linkedList.pop();
+console.log(linkedList.toString()); // (test1) -> (test2) -> null
+console.log(linkedList.contains("test1")); // true
+console.log(linkedList.find("test2")); // 1
+linkedList.prepend("test3");
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test2) -> null
+linkedList.insertAt("test4", 2);
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> null
+linkedList.insertAt("test5", 8);
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> (test5) -> null
+linkedList.removeAt(2);
+console.l;
